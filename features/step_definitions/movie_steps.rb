cd2c5bca4e61901568f5086bd373a125123dfa1b
GordonFern <gordon.fern@btinternet.com>
2013-09-29 22:46:34 +0100

    rspec tests covering controller

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,60 @@ Given /^the following movies exist:$/ do |movies_table|
   end
 end
 
-Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |arg1,arg2|
+ Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |arg1,arg2|
   the_movie=Movie.find_all_by_title(arg1)
   the_movie[0].director = arg2
 end
 
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  pos1=page.body.index(e1)
+  pos1=(pos1==nil) ? 0 : pos1
+  pos2=page.body.index(e2)
+  pos2=(pos2==nil) ? 0 : pos2
+  pos2.should>pos1
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  test_arr=rating_list.split(',')
+  test_arr.each do | rating |
+    if uncheck == 'un'
+      uncheck('ratings_' + rating.strip)
+    else
+      check('ratings_' + rating.strip)
+    end
+  end
+end
+
+Given /^I submit ratings form$/ do
+  click_button('ratings_submit')
+end
+
+Then /^I should see (\d+) movies$/ do |arg1|
+  no_rows=-1 # Need to exclude header row
+  all('#movies tr').each do |td|
+    no_rows += 1
+  end
+
+  if arg1.strip != no_rows.to_s.strip
+    flunk "Expected " + arg1 + " rows - found " + no_rows.to_s
+  end
+end
+
+Then /I should see only ratings: (.*)/ do | rating_list|
+  str_arr=rating_list.gsub(/ /,'').split(',')
+  all('#movies tr > td:nth-child(2)').each do |td|
+    if !str_arr.include?(td.text)
+       flunk "Can't find #{td.text}"
+    end
+  end
+end
--- spec/controllers/movie_controller_spec.rb
+++ spec/controllers/movie_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 describe MoviesController do
   describe 'List movies by director' do
     it 'should searches movies by director and render movies_by_director' do
@@ -8,5 +9,125 @@ describe MoviesController do
       get :movies_by_director,{:director=>'alfonze'}
       response.should render_template :movies_by_director
     end
+    it 'should flash message & redirect to index if nil director' do
+      get :movies_by_director,{:movie_title=>'Testing'}
+      flash[:notice].should eq("'Testing' has no director info")
+      response.should redirect_to(action: 'index')
+    end
   end
+  describe 'Extract valid movie classifications' do
+    it 'should return the range of ratings' do
+       Movie.all_ratings.should  == %w(G PG PG-13 NC-17 R)
+     end
+
+  end
+  describe 'show movies' do
+    it 'should find the specified movie' do
+      fake_results = mock('Movie')
+      Movie.should_receive(:find).with("1").and_return(fake_results)
+      get :show,{:id=>1}
+      response.should render_template :show
+    end
+  end
+  describe 'Edit movie' do
+    it 'should edit the movie' do
+      fake_results=mock('Movie')
+       Movie.should_receive(:find).with("1").and_return(fake_results)
+       get :edit,{:id=>1}
+       response.should render_template :edit
+    end
+  end
+  describe 'Update Movie' do
+    it 'should find the movie and update the attributes' do
+      fake_results=mock_model('Movie')
+      fake_results.stub(:title).and_return('Hi')
+      fake_results.stub(:update_attributes!).with({'director'=>'fred','title'=>'Hi'}).and_return(true)
+      Movie.should_receive(:find).with("7").and_return(fake_results)
+      put :update,:id=>7,:movie=>{:director=>'fred',:title=>'Hi'}
+      flash[:notice].should eq('Hi was successfully updated.')
+      response.should redirect_to(fake_results)
+    end
+  end
+  describe 'Create Movie' do
+    it 'should create the movie' do
+      fake_results=mock_model('Movie')
+      fake_results.stub(:title).and_return('Hi')
+      Movie.should_receive(:create!).with({'director'=>'fred','title'=>'Hi'}).and_return(fake_results)
+      get :create,:id=>7,:movie=>{:director=>'fred',:title=>'Hi'}
+      flash[:notice].should eq('Hi was successfully created.')
+      response.should redirect_to(action: 'index')
+    end
+  end
+
+
+  describe 'Destroy Movie' do
+    it 'Should delete the move and flash a message' do
+       fake_results=mock('Movie')
+       fake_results.stub(:destroy).and_return(true)
+       fake_results.stub(:title).and_return('Testing')
+       Movie.should_receive(:find).with("1").and_return(fake_results)
+       get :destroy,{:id=>1}
+       flash[:notice].should eq("Movie 'Testing' deleted.")
+       response.should redirect_to(action: 'index')
+    end
+  end
+
+  describe 'Index List' do
+    context 'when no session settings' do
+      before(:each) do
+        session.clear
+        Movie.should_receive(:all_ratings).and_return(%w(G PG PG-13 NC-17 R))
+      end
+      it 'If there are no params then default all catergories' do
+        fake_results=mock_model('Movie')
+        Movie.should_receive('find_all_by_rating').with(%w(G PG PG-13 NC-17 R),nil).and_return(fake_results)
+        get :index
+        response.should render_template :index
+      end
+      it 'should sort by title if sort=>title' do
+        get :index,{:sort=>'title'}
+        session[:sort].should eq('title')
+        response.should redirect_to(:sort=>'title',:ratings=>{'G'=>'G','PG'=>'PG','PG-13'=>'PG-13','NC-17'=>'NC-17','R'=>'R'})
+      end
+      it 'should sort by release data if sort=>release_date' do
+        get :index,{:sort=>'release_date'}
+        session[:sort].should eq('release_date')
+        response.should redirect_to(:sort=>'release_date',:ratings=>{'G'=>'G','PG'=>'PG','PG-13'=>'PG-13','NC-17'=>'NC-17','R'=>'R'})
+      end
+      it 'should restrict by selected ratings' do
+        get :index,{:ratings=>{'G'=>'G','PG'=>'PG'}}
+        session[:ratings].should eq({'G'=>'G','PG'=>'PG'})
+        response.should redirect_to(:ratings=>{'G'=>'G','PG'=>'PG'})
+        
+      end
+    end
+    context 'search session settings' do
+      before(:each) do
+        Movie.should_receive(:all_ratings).and_return(%w(G PG PG-13 NC-17 R))
+        session[:ratings]={'G'=>'G','PG'=>'PG','PG-13'=>'PG-13','NC-17'=>'NC-17','R'=>'R'}
+        session[:sort]='title'
+      end
+      it 'If there are no params then default all catergories' do
+        fake_results=mock_model('Movie')
+        get :index
+        response.should redirect_to(:sort=>'title',:ratings=>{'G'=>'G','PG'=>'PG','PG-13'=>'PG-13','NC-17'=>'NC-17','R'=>'R'})
+      end
+      it 'should sort by title if sort=>title' do
+        get :index,{:sort=>'title'}
+        session[:sort].should eq('title')
+        response.should redirect_to(:sort=>'title',:ratings=>{'G'=>'G','PG'=>'PG','PG-13'=>'PG-13','NC-17'=>'NC-17','R'=>'R'})
+      end
+      it 'should sort by release data if sort=>release_date' do
+        get :index,{:sort=>'release_date'}
+        session[:sort].should eq('release_date')
+        response.should redirect_to(:sort=>'release_date',:ratings=>{'G'=>'G','PG'=>'PG','PG-13'=>'PG-13','NC-17'=>'NC-17','R'=>'R'})
+      end
+      it 'should restrict by selected ratings' do
+        get :index,{:ratings=>{'G'=>'G','PG'=>'PG'}}
+        session[:ratings].should eq({'G'=>'G','PG'=>'PG','PG-13'=>'PG-13','NC-17'=>'NC-17','R'=>'R'})
+        response.should redirect_to(:ratings=>{'G'=>'G','PG'=>'PG'},:sort=>'title')
+      end
+    end
+  end
+
 end
